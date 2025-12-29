a9bd9194e209ed5b599dd83e45c7aa1e6ea668a1
Dhruvi Agrawal <dhruviswayam@gmail.com>
2025-12-30 02:40:16 +0530

    Update tb_cic.v

--- DSM/testbench/individual/tb_cic.v
+++ DSM/testbench/individual/tb_cic.v
@@ -1,4 +1,12 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1fs
+/* FIR filter implementation on FPGA
+ * Module: testbench for the 5th order cic filter
+ * Description:
+ * The input signal is the output of Delta Sigma Modulator from MATLAB provided in the form of a text file
+ *
+ * Engineer: Dhruvi A
+ * Date: 30/12/25
+ */
 
 module tb_cic;
 
